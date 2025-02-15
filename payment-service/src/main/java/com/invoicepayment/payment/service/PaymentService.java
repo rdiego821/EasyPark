c91ae97a8b2612cf502472cd6dac3327d5078414
Diego Acero <diego_acero@epam.com>
2025-02-15 13:06:21 -0500

    Refactor Payment Service

--- payment-service/src/main/java/com/invoicepayment/payment/service/PaymentService.java
+++ payment-service/src/main/java/com/invoicepayment/payment/service/PaymentService.java
@@ -1,6 +1,5 @@
 package com.invoicepayment.payment.service;
 
-import com.invoicepayment.payment.exception.PaymentException;
 import com.invoicepayment.payment.model.Payment;
 import com.invoicepayment.payment.model.PaymentMethod;
 import com.invoicepayment.payment.strategy.PaymentStrategy;
